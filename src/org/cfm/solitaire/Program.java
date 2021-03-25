4d552c105bcb2d2884a6643af3207cdfd06e168a
festafolcm <carlos.fmendu@gmail.com>
2021-03-25 20:00:33 +0100

    Baraja y Carta creada. Trabajando en Mesa.

--- src/org/cfm/solitaire/Program.java
+++ src/org/cfm/solitaire/Program.java
@@ -1,7 +1,11 @@
 package org.cfm.solitaire;
 
+import org.cfm.solitaire.models.Carta;
+import org.cfm.solitaire.models.Mesa;
+
 public class Program {
     public static void main(String[] args) {
 
     }
+
 }
